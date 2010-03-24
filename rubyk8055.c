235677d3ffeb93bfbebdde9c3afa55b9197a1577
Nathan Broadbent <nathan.f77@gmail.com>
2010-03-25 09:04:38 +1300

    Heavily fixed rubyk8055 c class

--- rubyk8055.c
+++ rubyk8055.c
@@ -13,9 +13,6 @@
 
 // Set default values.
 
-int connected = false;
-int ipid = 0;
-
 int delay = 0;
 
 // Defining a space for information and references about the module to be stored internally
@@ -26,8 +23,8 @@ void Init_rubyk8055();
 
 // ------------------- Validations ---------------------
 
-int check_connection(void) {
-    if (connected == true) {
+static int check_connection(VALUE self) {
+    if (rb_iv_get(self, "@connected") == true) {
         return true;
     } else {
         printf("Not connected to K8055!\n");
@@ -35,7 +32,7 @@ int check_connection(void) {
     }
 }
 
-int valid_analog_channel(long channel) {
+static int valid_analog_channel(long channel) {
     if (channel == 1 || channel == 2) {
         return true;
     } else {
@@ -44,7 +41,7 @@ int valid_analog_channel(long channel) {
     }
 }
 
-int valid_analog_value(long value) {
+static int valid_analog_value(long value) {
     if (value >= 0 && value <= 255) {
         return true;
     } else {
@@ -53,7 +50,7 @@ int valid_analog_value(long value) {
     }
 }
 
-int valid_digital_channel(long channel) {
+static int valid_digital_channel(long channel) {
     if (channel >= 1 && channel <= 8) {
         return true;
     } else {
@@ -62,7 +59,7 @@ int valid_digital_channel(long channel) {
     }
 }
 
-int valid_counter(long counter) {
+static int valid_counter(long counter) {
     if (counter == 1 || counter == 2) {
         return true;
     } else {
@@ -73,25 +70,23 @@ int valid_counter(long counter) {
 
 // ----------------------------- K8055 Methods ---------------------------
 
-VALUE method_connect(int argc, VALUE *argv, VALUE self) {
-    long board_address;
+static VALUE method_connect(int argc, VALUE *argv, VALUE self) {
+    long board_address = NUM2INT(rb_iv_get(self, "@board_address"));
     // if no args given, connect to board '0'
-    if (argc == 0) {
-        board_address = 0;
-    } else {
+    if (argc > 0) {
         // Converts ruby fixnum to C data
         board_address = NUM2INT(argv[0]);
     }
-    if (connected == false) {
-        if (OpenDevice(board_address) > 0) {
+    if (rb_iv_get(self, "@connected") == false) {
+        if (OpenDevice(board_address) != -1) {
             printf("Connected to K8055 with address: %ld\n", board_address);
-            // Sets the 'ipid' to the connected board_address
-            ipid = board_address;
-            connected = true;
-            return 0;
+            rb_iv_set(self, "@connected", Qtrue);
+            // Sets the board address to the connected board.
+            rb_iv_set(self, "@board_address", board_address);
+            return Qtrue;
         } else {
             printf("Could not connect to K8055 with address: %ld\n", board_address);
-            connected = false;
+            rb_iv_set(self, "@connected", false);
         }
     } else {
         printf("Already connected to K8055!\n");
@@ -99,11 +94,11 @@ VALUE method_connect(int argc, VALUE *argv, VALUE self) {
     return false;
 }
 
-VALUE method_disconnect(void) {
-    if (check_connection()) {
-        if (CloseDevice() > 0) {
+static VALUE method_disconnect(VALUE self) {
+    if (check_connection(self)) {
+        if (CloseDevice() != -1) {
             printf("Closed connection to K8055.\n");
-            connected = false;
+            rb_iv_set(self, "@connected", false);
             return 0;
         } else {
             printf("Could not close connection to K8055.\n");
@@ -112,15 +107,15 @@ VALUE method_disconnect(void) {
     return false;
 }
 
-VALUE method_address(void) {
-    if (check_connection()) {
-        return INT2NUM(ipid); // int2num returns 0 instead of false.
+static VALUE method_address(VALUE self) {
+    if (check_connection(self)) {
+        return INT2NUM(rb_iv_get(self, "@board_address")); // int2num returns 0 instead of false.
     }
     return false;
 }
 
-VALUE method_get_analog(VALUE self, long channel) {
-    if (check_connection()) {
+static VALUE method_get_analog(VALUE self, long channel) {
+    if (check_connection(self)) {
         channel = NUM2INT(channel);
         if (valid_analog_channel(channel)) {
             long data = ReadAnalogChannel(channel);
@@ -132,8 +127,8 @@ VALUE method_get_analog(VALUE self, long channel) {
     }
 }
 
-VALUE method_set_analog(VALUE self, long channel, long value) {
-    if (check_connection()) {
+static VALUE method_set_analog(VALUE self, long channel, long value) {
+    if (check_connection(self)) {
         channel = NUM2INT(channel);
         value = NUM2INT(value);
         if (valid_analog_value(value)) {
@@ -147,16 +142,16 @@ VALUE method_set_analog(VALUE self, long channel, long value) {
     }
 }
 
-VALUE method_set_analog_max(VALUE self, long channel) {
+static VALUE method_set_analog_max(VALUE self, long channel) {
     return method_set_analog(self, channel, 255);
 }
 
-VALUE method_set_analog_min(VALUE self, long channel) {
+static VALUE method_set_analog_min(VALUE self, long channel) {
     return method_set_analog(self, channel, 0);
 }
 
-VALUE method_get_digital(VALUE self, long channel) {
-    if (check_connection()) {
+static VALUE method_get_digital(VALUE self, long channel) {
+    if (check_connection(self)) {
         channel = NUM2INT(channel);
         if (valid_digital_channel(channel)) {
             long data = ReadDigitalChannel(channel);
@@ -168,8 +163,8 @@ VALUE method_get_digital(VALUE self, long channel) {
     }
 }
 
-VALUE method_set_digital(VALUE self, long channel, int value) {
-    if (check_connection()) {
+static VALUE method_set_digital(VALUE self, long channel, int value) {
+    if (check_connection(self)) {
         channel = NUM2INT(channel);
         if (value != 0) {   // simple boolean conversion from non-zero to true
             value = true;
@@ -188,8 +183,8 @@ VALUE method_set_digital(VALUE self, long channel, int value) {
     }
 }
 
-VALUE method_write_all_digital(VALUE self, int value) {
-    if (check_connection()) {
+static VALUE method_write_all_digital(VALUE self, int value) {
+    if (check_connection(self)) {
         value = NUM2INT(value);
         if (WriteAllDigital(value) != -1)
             return true;
@@ -198,8 +193,8 @@ VALUE method_write_all_digital(VALUE self, int value) {
     }
 }
 
-VALUE method_set_all_digital(void) {
-    if (check_connection()) {
+static VALUE method_set_all_digital(VALUE self) {
+    if (check_connection(self)) {
         if (SetAllDigital() != -1)
             return true;
         printf("K8055 returned an error.\n");
@@ -207,8 +202,8 @@ VALUE method_set_all_digital(void) {
     }
 }
 
-VALUE method_clear_all_digital(void) {
-    if (check_connection()) {
+static VALUE method_clear_all_digital(VALUE self) {
+    if (check_connection(self)) {
         if (ClearAllDigital() != -1)
             return true;
         printf("K8055 returned an error.\n");
@@ -216,8 +211,8 @@ VALUE method_clear_all_digital(void) {
     }
 }
 
-VALUE method_set_all_analog(void) {
-    if (check_connection()) {
+static VALUE method_set_all_analog(VALUE self) {
+    if (check_connection(self)) {
         if (SetAllAnalog() != -1)
             return true;
         printf("K8055 returned an error.\n");
@@ -225,8 +220,8 @@ VALUE method_set_all_analog(void) {
     }
 }
 
-VALUE method_clear_all_analog(void) {
-    if (check_connection()) {
+static VALUE method_clear_all_analog(VALUE self) {
+    if (check_connection(self)) {
         if (ClearAllAnalog() != -1)
             return true;
         printf("K8055 returned an error.\n");
@@ -234,8 +229,8 @@ VALUE method_clear_all_analog(void) {
     }
 }
 
-VALUE method_read_counter(VALUE self, long counter) {
-    if (check_connection()) {
+static VALUE method_read_counter(VALUE self, long counter) {
+    if (check_connection(self)) {
         counter = NUM2INT(counter);
         if (valid_counter(counter)) {
             long data = ReadCounter(counter);
@@ -247,8 +242,8 @@ VALUE method_read_counter(VALUE self, long counter) {
     }
 }
 
-VALUE method_reset_counter(VALUE self, long counter) {
-    if (check_connection()) {
+static VALUE method_reset_counter(VALUE self, long counter) {
+    if (check_connection(self)) {
         counter = NUM2INT(counter);
         if (valid_counter(counter)) {
             if (ResetCounter(counter) != -1)
@@ -259,8 +254,8 @@ VALUE method_reset_counter(VALUE self, long counter) {
     }
 }
 
-VALUE method_set_debounce(VALUE self, long counter, long time) {
-    if (check_connection()) {
+static VALUE method_set_debounce(VALUE self, long counter, long time) {
+    if (check_connection(self)) {
         counter = NUM2INT(counter);
         time = NUM2INT(time);
         if (valid_counter(counter)) {
@@ -272,21 +267,27 @@ VALUE method_set_debounce(VALUE self, long counter, long time) {
     }
 }
 
-VALUE method_to_s(void) {
-    if (check_connection()) {
+static VALUE method_to_s(VALUE self) {
+    if (check_connection(self)) {
         return ReadAllValues();
     }
 }
 
 
-
-
+static VALUE rubyk8055Init(VALUE self) {
+  rb_iv_set(self, "@connected", false);
+  rb_iv_set(self, "@board_address", INT2NUM(0));
+}
 
 // ----------------------- RubyK8055 class initialization ----------------------
 
 void Init_rubyk8055() {
 
-    RubyK8055 = rb_define_class("RubyK8055", rb_cObject);
+    VALUE USB = rb_define_module("USB");
+    VALUE RubyK8055 = rb_define_class_under(USB, "RubyK8055", rb_cObject);
+
+    rb_define_method(RubyK8055, "initialize", rubyk8055Init, 0);
+
     rb_define_method(RubyK8055, "connect", method_connect, -1);
     rb_define_method(RubyK8055, "disconnect", method_disconnect, 0);
     rb_define_method(RubyK8055, "address", method_address, 0);
@@ -312,8 +313,6 @@ void Init_rubyk8055() {
     rb_define_method(RubyK8055, "set_debounce", method_set_debounce, 2);
 
 }
-
-
 
 
 /*
